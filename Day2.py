ff1712b64f6b9fdb9fb3cb10946cdc1dbb6d6043
Kabiir Krishna <kabiirk@gmail.com>
2020-12-02 15:45:48 +0530

    Create Day2.py

---
+++ Day2.py
@@ -0,0 +1,12 @@
+day2 = open("input_data/day2.txt", "r")
+
+count = 0
+
+for line in day2:
+    l = re.findall(r'(\d+)-(\d+)\s(.):\s(.*)',line) # [('15', '16', 'p', 'ppppppppppplppppp')]
+    instance_of_letter = l[0][3].count(l[0][2]) # no. of 'p' in 'ppppppppppplppppp'
+    if( instance_of_letter >= int(l[0][0]) and instance_of_letter <= int(l[0][1]) ): #Validation condition
+        count += 1 #increment counter 
+data.close()
+
+print(count)
