f323fd072cf322351e358c1dc6c507796717c465
Kabiir <kabiirk@gmail.com>
2020-12-04 16:03:28 +0530

    Day4 part 1 solution(prev commit) + comments

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -1,18 +1,20 @@
+#open File
 day4 = open("day4.txt", "r")
 
-count = 0
+#combine all entries in 1 string
 entries = ""
-
 for line in day4:
     entries += line
 day4.close()
 
-passports = entries.split("\n\n")
-fields = [ "byr", "iyr" , "eyr" ,"hgt" ,"hcl" ,"ecl" ,"pid"]
+#Split into list with empty line as delimiter which is essentially 2 newlines
+passports = entries.split("\n\n") # => [password1, poassword2, ......]
+fields = [ "byr", "iyr" , "eyr" ,"hgt" ,"hcl" ,"ecl" ,"pid"] #Fields present
 counter = 0
 
+#Count valid passwords
 for passport in passports:
     passport.replace("\n", " ")
-    if all(substring in passport for substring in fields):
+    if all(substring in passport for substring in fields): #Password validation condition as per question
         counter+=1
 print(counter)
