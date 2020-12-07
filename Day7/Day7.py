3d3c18e6cd188c60d54b3e286bf5f5dd6d965f0e
Kabiir <kabiirk@gmail.com>
2020-12-07 15:33:09 +0530

    Day7 Sol + Readme + Data

---
+++ Day7/Day7.py
@@ -0,0 +1,28 @@
+from collections import defaultdict, deque
+
+# Reading input
+filename = "Day7.txt"
+rules = {}
+
+with open(filename,'r') as file_:
+    for line in file_:
+        s = line.strip().split(' bags contain ')
+        
+        content = defaultdict(int)
+        for comp in s[1].split(', '):
+            words = comp.split(' ')
+            if words[0] != 'no':
+                content[words[1] + ' ' + words[2]] = int(words[0])
+        rules[s[0]] = content
+
+
+bags = set(['shiny gold'])
+l = 0
+
+# Continue to execute this function until no new items are added to the set bags
+while len(bags) > l:
+    l = len(bags)
+    # Iterate over the rules, if the rule contains anything in the set bags, add the key for that rule to the set bags
+    [bags.add(key) for key in rules if any(color in rules[key].keys() for color in bags)]
+    
+print(f'Answer part 1: {len(bags)-1}')
